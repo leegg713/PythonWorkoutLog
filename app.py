6191b9ebcbbeb335c426d282f377a0015418af6d
Lee Merigold <leemerigold7@gmail.com>
2025-06-12 15:56:43 +0000

    Renamed Main.py to main.py and instantiated the app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# This page will be how the actual work is done by using the functions in Main.py
+
+# app.py
+from flask import Flask, render_template, request
+from main import display_menu  # import your logic
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    #name = request.args.get("name", "World")  # default to "World" if no query param
+    menu = display_menu()
+    return render_template("index.html", menu = menu)
+
+if __name__ == "__main__":
+    app.run(debug=True)
